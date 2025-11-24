47223cee63f69f69b2f19eac8e28803d14c80008
Franchesco <fbronchaly@gmail.com>
2025-11-24 12:30:28 +0100

    configuro .env para produccion

--- calidad_front_V2.0/src/environments/environment.prod.ts
+++ calidad_front_V2.0/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://193.147.197.113:8083'
+    apiUrl: '' // URL relativa para same-origin cuando se sirve desde el mismo backend
 };
